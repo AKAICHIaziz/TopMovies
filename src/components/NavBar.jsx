8fb70401b6700975acfec7e4d72e4f1adadce122
Med Aziz Akaichi <135503030+AKAICHIaziz@users.noreply.github.com>
2024-06-03 21:06:53 +0100

    aaa

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import NavBarItem from './NavBarItem'
+import { Suspense } from 'react'
 
 export default function NavBar() {
     return (
         <div className='flex dark:bg-gray-600 bg-amber-100 p-4 lg:text-lg justify-center gap-6'>
-            <NavBarItem title='Trending' param='fetchTrending' />
-            <NavBarItem title='Top Rated' param='fetchTopRated' />
+            <Suspense>
+                <NavBarItem title='Trending' param='fetchTrending' />
+                <NavBarItem title='Top Rated' param='fetchTopRated' />
+            </Suspense>
         </div>
     )
 }
