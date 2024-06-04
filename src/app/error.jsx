ca8512f8180b3052e4eba289cdcd32d76686d565
Med Aziz Akaichi <135503030+AKAICHIaziz@users.noreply.github.com>
2024-06-04 19:48:43 +0100

    res err 5

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useEffect } from "react"
+import { useEffect, Suspense } from "react"
 
 export default function Error({ error, reset }) {
 
@@ -9,9 +9,11 @@ export default function Error({ error, reset }) {
     }, [error])
 
     return (
-        <div className="text-center mt-10">
-            <h1>Something went wrong, please try again.</h1>
-            <button className="hover:text-amber-800" onClick={() => reset()}>Go back</button>
-        </div>
+        <Suspense fallback={<div>Loading...</div>}>
+            <div className="text-center mt-10">
+                <h1>Something went wrong, please try again.</h1>
+                <button className="hover:text-amber-800" onClick={() => reset()}>Go back</button>
+            </div>
+        </Suspense>
     )
 }
--- src/components/NavBarItem.jsx
+++ src/components/NavBarItem.jsx
@@ -1,25 +1,23 @@
 "use client"
 
 import Link from 'next/link'
-import React, { Suspense } from 'react'
+import React from 'react'
 import { useSearchParams } from 'next/navigation'
 
 export default function NavBarItem({ title, param }) {
     const searchParams = useSearchParams()
     const genre = searchParams.get('genre')
     return (
-        <Suspense fallback={<div>Loading ...</div>}>
-            <div>
-                <Link
-                    className=
-                    {
-                        `hover:text-red-600 font-semibold ${genre === param ? 'underline underline-offset-8 decoration-4 decoration-red-600 rounded-lg' : ''}`
-                    }
-                    href={`/?genre=${param}`}
-                >
-                    {title}
-                </Link>
-            </div>
-        </Suspense>
+        <div>
+            <Link
+                className=
+                {
+                    `hover:text-red-600 font-semibold ${genre === param ? 'underline underline-offset-8 decoration-4 decoration-red-600 rounded-lg' : ''}`
+                }
+                href={`/?genre=${param}`}
+            >
+                {title}
+            </Link>
+        </div>
     )
 }
